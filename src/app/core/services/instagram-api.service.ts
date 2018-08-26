56d66acca7b6d8bc33e720d7620dfcf615928683
Murena <andrei.burmitskii@digitalhanger.com>
2018-08-26 14:30:26 +0300

    Dev 0.0.06

--- src/app/core/services/instagram-api.service.ts
+++ src/app/core/services/instagram-api.service.ts
@@ -4,29 +4,54 @@ import { environment } from '../../../environments/environment';
 import { UserMedia } from '../../shared/types/userMedia';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { OwnerData } from '../../shared/types/ownerData';
 
 const USER_MEDIA_URL_PREFIX = 'users/self/media/recent/?access_token=';
+const OWNER_DATA_URL_PREFIX = 'users/self/?access_token=';
 
 @Injectable()
 export class InstagramApiService {
-  public ArrayUserMedia: Array<UserMedia>;
+  public userMedia: Array<UserMedia>;
+  public ownerData: OwnerData;
 
   constructor(private http: Http) {
     this.getUserMedia();
+    this.getOwnerData();
   }
 
   public getUserMedia() {
     this.requestUserMedia().subscribe(
-      (response) => (this.ArrayUserMedia = response),
+      (response) => (this.userMedia = response),
+    );
+  }
+
+  public getOwnerData() {
+    this.requestOwnerData().subscribe(
+      (response) => (this.ownerData = response),
     );
   }
 
   private requestUserMedia(): Observable<Array<UserMedia>> {
-    const ArrayUserMedia = this.http
+    const data = this.http
       .get(environment.instagramApiUrl + USER_MEDIA_URL_PREFIX + environment.instagramStaticAccessToken)
       .pipe(
         map(this.extractUserMedia)
       );
+    return data;
+  }
+
+  private requestOwnerData(): Observable<OwnerData> {
+    const ownerdata = this.http
+      .get(environment.instagramApiUrl + OWNER_DATA_URL_PREFIX + environment.instagramStaticAccessToken)
+      .pipe(
+        map(this.extractOwnerData)
+      );
+    return ownerdata;
+  }
+
+  private extractOwnerData (response: Response): OwnerData {
+    const res = response.json();
+    const ArrayUserMedia: OwnerData = res.data;
     return ArrayUserMedia;
   }
 
--- src/app/shared/timelinestoriesblock/timelinestoriesblock.component.ts
+++ src/app/shared/timelinestoriesblock/timelinestoriesblock.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OwnerData } from '../types/ownerData';
 
 @Component({
   selector: 'app-timelinestoriesblock',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class TimelinestoriesblockComponent implements OnInit {
 
   constructor() { }
+
+  @Input() ownerData: OwnerData;
 
   ngOnInit() {
   }
